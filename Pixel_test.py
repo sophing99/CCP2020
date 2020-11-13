b49e817ce068a21de252dc067ddcbab7800f8395
sophing <sophing99@github.com>
2020-11-13 12:48:39 +0900

    2차 수정

--- Pixel_test.py
+++ Pixel_test.py
@@ -2,19 +2,29 @@
 # coding: utf-8
 
 import cv2
-import Do_Tesseract
+import os
 
 
+def isdir(dir_name):
+    if not (os.path.isdir(dir_name)):
+        os.mkdir(dir_name)
+    return
+        
 def img_resize_small(img): #축소
     img_raw = cv2.imread(img, cv2.IMREAD_COLOR)
     height, width = img_raw.shape[0:2]
     resized_image = []
     
+    img_name = img.split('/')
+    img_name = img_name[len(img_name)-1].split('.')[0]
+    
+    isdir('./data/{}_resize'.format(img_name))
+    
     for i in range (1, 6): #마지막 6은 원본 크기
         resize_height = int(0.2*i*height)
         resize_width = int(0.2*i*width)
-        resized_image.append(cv2.resize(img_raw, (resize_width, resize_height), interpolation=cv2.INTER_CUBIC))
-    return resized_image
+        cv2.imwrite('./data/{}_resize/{}_small_{}.jpg'.format(img_name, img_name, i),cv2.resize(img_raw, (resize_width, resize_height), interpolation=cv2.INTER_CUBIC))
+    return
 
 
 def img_resize_large(img): #확대
@@ -22,25 +32,15 @@ def img_resize_large(img): #확대
     height, width = img_raw.shape[0:2]
     resized_image = []
     
+    img_name = img.split('/')
+    img_name = img_name[len(img_name)-1].split('.')[0]
+    
+    isdir('./data/{}_resize'.format(img_name))
+    
     for i in range (1, 6):
         resize_height = int((1+i)*height)
         resize_width = int((1+i)*width)
-        resized_image.append(cv2.resize(img_raw, (resize_width, resize_height), interpolation=cv2.INTER_CUBIC))
-    return resized_image
+        cv2.imwrite('./data/{}_resize/{}_large_{}.jpg'.format(img_name, img_name, i),cv2.resize(img_raw, (resize_width, resize_height), interpolation=cv2.INTER_CUBIC))
+    return
 
 
-
-def my_function(img):
-    result_small = []
-    result_large = []
-    
-    small = img_resize_small(img)
-    large = img_resize_large(img)
-    
-    for img in (small):
-        result_small.append(Do_Tesseract.do_tesseract(img))
-        
-    for img in (large):
-        result_large.append(Do_Tesseract.do_tesseract(img))
-        
-    return result_small, result_large
