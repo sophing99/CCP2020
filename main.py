031e46beb1f155c25ac80a1e2c8943cc785c76a1
seungmin <h.physics77@gmail.com>
2020-11-14 21:06:24 +0900

    main function

---
+++ main.py
@@ -0,0 +1,34 @@
+import os, sys
+import glob
+
+import argparse
+from tesseract.tesseract import *
+from preprocess.zoom import *
+
+parser = argparse.ArgumentParser(description="Implementation of ocr")
+
+#########################
+####### parameters ######
+#########################
+parser.add_argument("-img", "--input_img", default="./input/test_01.jpg", type=str, help="Input image path")
+parser.add_argument("-z", "--zoom_parameter", default=2, type=float, help="Zoom in/out parameter")
+parser.add_argument("-show", "--show_image", default=False, type=bool, help="Show zoom in/out image")
+
+
+def main():
+    global args
+    args = parser.parse_args()
+
+    ## preprocessing
+    zoom(args.input_img, args.zoom_parameter, args.show_image)
+    #new_img = glob.glob("./input/zoom_*")
+    ## tesseract
+    img_to_str("./input/zoom_test_01.jpg")
+
+
+    return
+
+
+if __name__ == "__main__":
+    main()
+
